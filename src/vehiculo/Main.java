ca4bb78aba2f4d8c951db78e23a11e89363d38e4
juand <juand@SOBREMESA>
2023-02-19 13:01:31 +0100

    Mi primer commit LobatonMateosJuanJosé2223

---
+++ src/vehiculo/Main.java
@@ -0,0 +1,41 @@
+
+package vehiculo;
+
+/**
+ *
+ * @author IES Aguadulce
+ */
+public class Main {
+    
+      public static void main(String[] args) {
+        VehiculoLobatonMateosJuanJose2223 miVehiculoLobatonMateosJuanJose2223;
+        int stockActual;
+        
+        miVehiculoLobatonMateosJuanJose2223 = new VehiculoLobatonMateosJuanJose2223("Seat",18000,100);
+        operativaVehiculosLobatonMateosJuanJose2223(miVehiculoLobatonMateosJuanJose2223); 
+        stockActual = miVehiculoLobatonMateosJuanJose2223.obtenerStock();
+        System.out.println("El stock actual es"+ stockActual );
+    }
+
+    private static void operativaVehiculosLobatonMateosJuanJose2223(VehiculoLobatonMateosJuanJose2223 miVehiculoLobatonMateosJuanJose2223) {
+        try
+        {
+            System.out.println("Venta de Vehiculos");
+            miVehiculoLobatonMateosJuanJose2223.vender(20);
+        } catch (Exception e)
+        {
+            System.out.print("Fallo al vender");
+        }
+        
+        try
+        {
+            System.out.println("Compra de Vehiculos");
+            miVehiculoLobatonMateosJuanJose2223.comprar(100);
+        } catch (Exception e)
+        {
+            System.out.print("Fallo al comprar");
+        }
+    }
+
+}
+    
